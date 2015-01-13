e878a1a9e1e2e95846f2397726b9e231ece1906b
Tim Shedor <tim@finedesigngroup.com>
2015-01-13 13:55:24 -0800

    include wraith images in raw output

--- lib/maximus/statistics/wraith.rb
+++ lib/maximus/statistics/wraith.rb
@@ -3,6 +3,20 @@ module Maximus
   class Wraith < Maximus::Statistic
 
     # Generates screenshots for visual regression testing
+    #
+    # WARNING: If you call this class from a script,
+    #   you should delete the images generated after they've been
+    #   created.
+    # @example removing images after run
+    #   dest = '/desired/root/directory'
+    #   wraith_pics = Maximus::Wraith.new.result
+    #   wraith_pics.each do |path_label, data|
+    #     data[:images].each do |image|
+    #       moved_image = "#{dest}/#{File.basename(image)}"
+    #       FileUtils.mv image, moved_image, force: true
+    #     end
+    #   end
+    #
     # @see Statistic#initialize
     def result
 
@@ -14,10 +28,6 @@ module Maximus
 
       # Run history or latest depending on the existence of a history directory as defined
       #   in each wraith config file.
-      #
-      # @todo this doesn't work very well. It puts the new shots in the history folder,
-      #   even with absolute paths. Could be a bug in wraith
-      #
       # @yieldparam browser [String] headless browser name
       # @yieldparam configpath [String] path to temp config file (see Config#wraith_setup)
       @settings[:wraith].each do |browser, configpath|
@@ -25,10 +35,15 @@ module Maximus
         wraith_yaml = YAML.load_file(configpath)
         if File.directory?("#{@settings[:root_dir]}/#{wraith_yaml['history_dir']}")
           puts `wraith latest #{configpath}`
+
           # Reset history dir
+          # It puts the new shots in the history folder,
+          #   even with absolute paths in the config.
+          #   Could be a bug in wraith
           puts `rm -rf #{@settings[:root_dir]}/#{wraith_yaml['history_dir']}`
         end
         wraith_parse browser
+        wraith_images browser unless @config.is_dev?
         puts `wraith history #{configpath}`
       end
       @output
@@ -47,6 +62,7 @@ module Maximus
         Dir.glob("#{@settings[:root_dir]}/maximus_wraith_#{browser}/**/*.txt").select { |f| File.file? f }.each do |file|
           file_object = File.open(file, 'rb')
           orig_label = File.dirname(file).split('/').last
+          # Get stored key value
           label = @settings[:paths][orig_label]
           @output[:statistics][label.to_s] ||= {}
           browser_output = @output[:statistics][label.to_s]
@@ -56,6 +72,35 @@ module Maximus
           browser_output[:percent_changed] ||= {}
           browser_output[:percent_changed][File.basename(file).split('_')[0].to_i] = file_object.read.to_f
           file_object.close
+        end
+        @output
+      end
+
+      # Make images temp files to save for later
+      # @todo DRY this up from wraith_parse
+      # @since 0.1.5
+      # @param browser [String]
+      def wraith_images(browser)
+        Dir.glob("#{@settings[:root_dir]}/maximus_wraith_#{browser}/**/*.png").select { |f| File.file? f }.each do |file|
+          file_object = File.open(file, 'rb')
+          orig_label = File.dirname(file).split('/').last
+          # Get stored key value
+          label = @settings[:paths][orig_label]
+          @output[:statistics][label.to_s] ||= {}
+          browser_output = @output[:statistics][label.to_s]
+          browser_output ||= {}
+          browser_output[:images] ||= []
+          data = File.read(file)
+          image = Tempfile.new([File.basename(file), '.png']).tap do |f|
+            f.rewind
+            f.write(data)
+            # Once this script exits, the images will delete themselves
+            # We need those images, so undefine that last call
+            # http://stackoverflow.com/a/21286718
+            ObjectSpace.undefine_finalizer(f)
+            f.close
+          end
+          browser_output[:images] << image.path
         end
         @output
       end
