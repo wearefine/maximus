bfa3e7e321f190d7e2191afdca51d2725d0420ee
Tim Shedor <tim@finedesigngroup.com>
2015-01-20 11:47:00 -0800

    remove extraneous diff-lines function

--- lib/maximus/reporter/git-lines.sh
+++ lib/maximus/reporter/git-lines.sh
@@ -59,25 +59,6 @@ function lines-added(){
     fi
   done
 }
-function diff-lines(){
-  local path=
-  local line=
-  while read; do
-    esc=$'\033'
-    if [[ $REPLY =~ ---\ (a/)?.* ]]; then
-      continue
-    elif [[ $REPLY =~ \+\+\+\ (b/)?([^[:blank:]$esc]+).* ]]; then
-      path=${BASH_REMATCH[2]}
-    elif [[ $REPLY =~ @@\ -[0-9]+(,[0-9]+)?\ \+([0-9]+)(,[0-9]+)?\ @@.* ]]; then
-      line=${BASH_REMATCH[2]}
-    elif [[ $REPLY =~ ^($esc\[[0-9;]+m)*([\ +-]) ]]; then
-      echo "$path:$line:$REPLY"
-      if [[ ${BASH_REMATCH[2]} != - ]]; then
-        ((line++))
-      fi
-    fi
-  done
-}
 
 if [ -z "$2" ]; then
   first_commit=$(git rev-list --max-parents=0 HEAD)
