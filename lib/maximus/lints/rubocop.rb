f2e4a5a9177abac89af0bbb71fc84d983e1eb864
James Kurczodyna <james@wearefine.com>
2015-02-18 02:21:52 -0800

    rubocop should lint the entire app

--- lib/maximus/lints/rubocop.rb
+++ lib/maximus/lints/rubocop.rb
@@ -7,7 +7,7 @@ module Maximus
     # @see Lint#initialize
     def result
       @task = 'rubocop'
-      @path = is_rails? ? "#{@config.working_dir}/app" : "#{@config.working_dir}/*.rb" if @path.blank?
+      @path = is_rails? ? @config.working_dir : "#{@config.working_dir}/*.rb" if @path.blank?
 
       return unless temp_config(@task) && path_exists?(@path)
 
